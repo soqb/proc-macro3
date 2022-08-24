926d644fced11d0de6e949fa8677a2d6226e7d76
radiish <67161944+istanbul-not-constantinople@users.noreply.github.com>
2022-08-24 12:11:29 +0200

    cfg for proc_macro and proc_macro2

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#[cfg(proc_macro)]
+extern crate proc_macro;
+#[cfg(proc_macro)]
+pub use proc_macro::*;
+#[cfg(feature = "proc_macro2")]
+extern crate proc_macro2;
+#[cfg(feature = "proc_macro2")]
+pub use proc_macro2::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// do not allow use of this library without the "proc_macro2" flag outside of proc_macro contexts
+#[cfg(not(any(proc_macro, feature = "proc_macro2"))]
+core::compile_error!("this crate must be used either with the \"proc_macro2\" feature flag or in a proc_macro context");
